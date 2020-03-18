c2c103a2fae0df6447bf1fbb5d6a27a79acd60ea
Nandiyta <61815366+Nandiyta@users.noreply.github.com>
2020-03-18 17:26:52 +0700

    Add files via upload

---
+++ latihanjavaform2/Model/ModelProgram.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjavaform2.Model;
+
+/**
+ *
+ * @author Nanda Yunita
+ */
+public class ModelProgram {
+    public String label = "Please put your information to get the result";
+    
+    public String returnGenderNoun(String gender){
+        String noun = "He";
+        
+        if(gender.toLowerCase().equals("Female")){
+            noun = "She";
+        }
+        
+        return noun;
+    }
+
+    public String returnMental(String mental) {
+        switch(mental){
+            case ":)":
+                return "Smilling";
+            case ":D":
+                return "Happy";
+            case ":(":
+                return "Sad";
+            case ":'(":
+                return "Crying";
+            default:
+                return "In Love";
+            
+        }
+                
+        
+    }
+}
+
+
+
+
+
+
